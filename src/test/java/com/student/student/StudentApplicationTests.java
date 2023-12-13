b4e114cf25364391b5f42210910a03f157793ae9
saif <you@example.com>
2023-12-13 20:59:24 +0600

    simple solution

--- src/test/java/com/student/student/StudentApplicationTests.java
+++ src/test/java/com/student/student/StudentApplicationTests.java
@@ -42,7 +42,7 @@ class StudentApplicationTests {
 		studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
 		studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
 
-		List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
+		List<String> expectedResult = Arrays.asList( "Mahmud Hasan", "Nabil", "Sadia Afrose","Shyfuzzaman bhai",  "Joytee dada");
 		List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
 		Assertions.assertEquals(expectedResult, actualResult);
 	}
@@ -64,7 +64,7 @@ class StudentApplicationTests {
 		studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
 		studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
 
-		List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
+		List<String> expectedResult = Arrays.asList( "Mahmud Hasan", "Mahmud Hasan","Mahmud Hasan", "Nabil", "Sadia Afrose", "Sadia Afrose","Shyfuzzaman bhai",  "Joytee dada");
 		List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
 		Assertions.assertEquals(expectedResult, actualResult);
 	}
@@ -72,78 +72,85 @@ class StudentApplicationTests {
 
 	@Test
 	void negativeContainNullTest() {
-		studentDataPreparation.clear();
-		studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
-		studentDataPreparation.add(null);
-		studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(null);
-		studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
-		studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
-		studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
-		studentDataPreparation.add(null);
-		studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
-		studentDataPreparation.add(null);
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			studentDataPreparation.clear();
+			studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
+			studentDataPreparation.add(null);
+			studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(null);
+			studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
+			studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
+			studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
+			studentDataPreparation.add(null);
+			studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
+			studentDataPreparation.add(null);
 
-		List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
+			List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
 
-		List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
-		Assertions.assertEquals(expectedResult, actualResult);
+			List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
+			Assertions.assertEquals(expectedResult, actualResult);
+				});
+
 	}
 
 	@Test
 	void negativeContainNullGradeTest() {
-		studentDataPreparation.clear();
-		studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
-		studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
-		studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
-		studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
-		studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
-		// null case
-		studentDataPreparation.add(new Student("Rahul Dada", Optional.ofNullable(null)));
-		studentDataPreparation.add(new Student("Sarawer bhai", null));
-		studentDataPreparation.add(new Student("Hafiz bhai", Optional.ofNullable(new Grade(null))));
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			studentDataPreparation.clear();
+			studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
+			studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
+			studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
+			studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
+			studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
+			// null case
+			studentDataPreparation.add(new Student("Rahul Dada", Optional.ofNullable(null)));
+			studentDataPreparation.add(new Student("Sarawer bhai", null));
+			studentDataPreparation.add(new Student("Hafiz bhai", Optional.ofNullable(new Grade(null))));
 
 
-		studentDataPreparation.add(null);
+			studentDataPreparation.add(null);
 
-		List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
+			List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
 
-		List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
-		Assertions.assertEquals(expectedResult, actualResult);
+			List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
+			Assertions.assertEquals(expectedResult, actualResult);
+		});
+
 	}
 
 
 	@Test
 	void negativeUnexpectedGradeTest() {
-		studentDataPreparation.clear();
-		studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
-		studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
-		studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
-		studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
-		studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
-		studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
-		studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
-		// case 4
-		studentDataPreparation.add(new Student("Rahul Dada", Optional.of(new Grade("P"))));
-		studentDataPreparation.add(new Student("Sarawer bhai", Optional.of(new Grade("H"))));
-		studentDataPreparation.add(new Student("Hafiz bhai", Optional.of(new Grade("K"))));
+		Assertions.assertThrows(NullPointerException.class, () ->{
+			studentDataPreparation.clear();
+			studentDataPreparation.add(new Student("Saiful Islam", Optional.of(new Grade("D"))));
+			studentDataPreparation.add(new Student("Mahmud Hasan", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Nabil", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(new Student("Sadia Afrose", Optional.of(new Grade("C"))));
+			studentDataPreparation.add(new Student("Russel Bhai", Optional.of(new Grade("F"))));
+			studentDataPreparation.add(new Student("Masud bhai", Optional.of(new Grade("E"))));
+			studentDataPreparation.add(new Student("Shyfuzzaman bhai", Optional.of(new Grade("A"))));
+			studentDataPreparation.add(new Student("Joytee dada", Optional.of(new Grade("B"))));
+			studentDataPreparation.add(new Student("Ponir Dada", Optional.of(new Grade("F"))));
+			// case 4
+			studentDataPreparation.add(new Student("Rahul Dada", Optional.of(new Grade("P"))));
+			studentDataPreparation.add(new Student("Sarawer bhai", Optional.of(new Grade("H"))));
+			studentDataPreparation.add(new Student("Hafiz bhai", Optional.of(new Grade("K"))));
 
 
-		studentDataPreparation.add(null);
+			studentDataPreparation.add(null);
 
-		List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
+			List<String> expectedResult = Arrays.asList( "Sadia Afrose", "Nabil", "Joytee dada", "Mahmud Hasan", "Shyfuzzaman bhai");
 
-		List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
-		Assertions.assertEquals(expectedResult, actualResult);
+			List<String> actualResult = solution.getStudentsAboveGrade(studentDataPreparation);
+		});
 	}
 
 }
