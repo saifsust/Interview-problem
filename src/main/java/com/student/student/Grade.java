cc2f64dc2a3905342cc0010981697437f3b78a92
saif <you@example.com>
2023-12-13 19:56:27 +0600

    Distinct students according to Grade

---
+++ src/main/java/com/student/student/Grade.java
@@ -0,0 +1,13 @@
+package com.student.student;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class Grade {
+    private String value;
+}
---
+++ src/main/java/com/student/student/Solution.java
@@ -0,0 +1,22 @@
+package com.student.student;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public final class Solution {
+
+    public List<String> getStudentsAboveGrade (List<Student> students) {
+        Set<String> duplication = new HashSet<>();
+        return students
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(student -> Objects.nonNull(student.getGrade())) // Optional Grade object is not null
+                .filter(student -> student.getGrade().isPresent()) // Containing Object within Optional is not null
+                .filter(student -> Objects.nonNull(student.getGrade().get().getValue())) // Grade object's value is not null
+                .filter(student -> student.getGrade().get().getValue().compareTo("C") <= 0)
+                .map(student -> student.getName())
+                .distinct()
+                //.filter(name -> duplication.add(name))
+                .collect(Collectors.toList());
+    }
+}
