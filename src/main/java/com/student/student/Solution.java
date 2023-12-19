209b4028e9e2bdc243dd8e0fbc72e61f4e07621c
saif <you@example.com>
2023-12-19 21:40:27 +0600

    previous code clean up

--- src/main/java/com/student/student/Solution.java
+++ src/main/java/com/student/student/Solution.java
@@ -1,24 +1,6 @@
 package com.student.student;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public final class Solution {
 
-    public List<String> getStudentsAboveGrade (List<Student> students) {
-        Set<String> duplication = new HashSet<>();
-        return students
-                .stream()
-                .filter(Objects::nonNull)
-                .filter(student -> Objects.nonNull(student.getGrade())) // Optional Grade object is not null
-                .filter(student -> student.getGrade().isPresent()) // Containing Object within Optional is not null
-                .filter(student -> Objects.nonNull(student.getGrade().get().getValue())) // Grade object's value is not null
-                .filter(student -> student.getGrade().get().getValue().compareTo("C") <= 0)
-                .sorted((student1, student2)-> student2.getGrade().get().getValue()
-                        .compareTo(student1.getGrade().get().getValue()))
-                .map(student -> student.getName())
-                .distinct()
-                //.filter(name -> duplication.add(name))
-                .collect(Collectors.toList());
-    }
+
 }
