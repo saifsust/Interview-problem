2b16a7bed1a31c3dd53034c05c6ff619e920b78d
saif <you@example.com>
2023-12-19 21:52:42 +0600

    prime finding solution from a string

--- src/main/java/com/student/student/Solution.java
+++ src/main/java/com/student/student/Solution.java
@@ -1,6 +1,33 @@
 package com.student.student;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public final class Solution {
+    private static final String SEPARATOR = "/";
 
+    public List<Integer> getPrimes(String str) {
+        return Arrays.stream(str.split(SEPARATOR))
+                .map(String::trim)
+                .filter(Solution::isNumeric)
+                .map(Integer::parseInt)
+                .filter(Solution::isPrime)
+                .collect(Collectors.toList());
+    }
 
+    private static boolean isNumeric(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        }catch (Exception exception){
+         return false;
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 }
--- src/test/java/com/student/student/StudentApplicationTests.java
+++ src/test/java/com/student/student/StudentApplicationTests.java
@@ -1,8 +1,28 @@
 package com.student.student;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class StudentApplicationTests {
+    private final Solution solution = new Solution();
 
+    @Test
+    void positiveTest1() {
+        var input = "///2/3/ 4/  5/sfd/s/1424///11";
+        var expectedResult = Arrays.asList(2, 3, 5, 11);
+        var actualResult = solution.getPrimes(input);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void positiveTest2() {
+        var input = "///sfd/s/1424///11/df/31/fdkfj/97/76";
+        var expectedResult = Arrays.asList(11, 31, 97);
+        var actualResult = solution.getPrimes(input);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
